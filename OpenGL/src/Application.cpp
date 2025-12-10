b4a35e5cb51dd5e58b36317218a7edb442e518d7
Dmitry <Ivanovdmitry530@gmail.com>
2025-12-10 18:38:33 +0400

    Добавил шейдеры я, конечно, но чет хз

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -2,6 +2,49 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "shader!" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+        
+    return id;
+}
+
+static int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) // функция для создания шейдера
+{
+    // цель функции - сделать несколько вещей, но OpenGl нужно предоставить наш исходный код шейдера
+    unsigned int program = glCreateProgram();
+    // создаем шейдеры
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
 int main(void)
 {
     GLFWwindow* window;
@@ -62,7 +105,30 @@ int main(void)
                                                                            // (pointer = 0) — смещение внутри VBO, откуда начинается первый атрибут.
                                                                            //  0 означает "начинать прямо с начала данных"
                                                                            //  Это БАЙТОВОЕ смещение, не указатель на CPU-данные.
-    /* Loop until the user closes the window */
+
+    std::string vertexShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec2 position;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(position, 0.0, 1.0);\n"
+        "}\n";
+
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
+
+/* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
