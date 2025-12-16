a08e36a96634adabdf2a7c0e39a733f90eda36c9
Dmitry <Ivanovdmitry530@gmail.com>
2025-12-16 14:04:30 +0400

    Dealing with Errors in OpenGL

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -5,6 +5,27 @@
 #include <string>
 #include <sstream>
 
+#define ASSERT(x) if (!(x)) __debugbreak();
+#define GLCall(x) GLClearError();\
+    x;\
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+static void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR); // удаляем все предыдущие ошибки OpenGL
+
+}
+
+static bool GLLogCall(const char* function, const char* file, int line)
+{
+    while (GLenum error = glGetError()) // пока glGetError != 0
+    {
+        std::cout << "[OpenGL Error] " << error << function << " " << file << ":" << line << std::endl;
+        return false; // вызов не удался
+    }
+    return true;
+}
+
 struct ShaderProgramSource
 {
     std::string VertexSource;
@@ -47,11 +68,11 @@ static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
     unsigned int id = glCreateShader(type);
     const char* src = source.c_str();
-    glShaderSource(id, 1, &src, nullptr);
-    glCompileShader(id);
+    GLCall(glShaderSource(id, 1, &src, nullptr));
+    GLCall(glCompileShader(id));
 
     int result;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    GLCall(glGetShaderiv(id, GL_COMPILE_STATUS, &result));
     if (result == GL_FALSE)
     {
         int length;
@@ -75,10 +96,10 @@ static int CreateShader(const std::string& vertexShader, const std::string& frag
     unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
     unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
 
-    glAttachShader(program, vs);
-    glAttachShader(program, fs);
-    glLinkProgram(program);
-    glValidateProgram(program);
+    GLCall(glAttachShader(program, vs));
+    GLCall(glAttachShader(program, fs));
+    GLCall(glLinkProgram(program));
+    GLCall(glValidateProgram(program));
 
     glDeleteShader(vs);
     glDeleteShader(fs);
@@ -95,7 +116,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Window_OpenGL", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -123,23 +144,23 @@ int main(void)
     };
 
     unsigned int buffer;
-    glGenBuffers(1, &buffer); // Создаем буффер и получаем его ID
+    GLCall(glGenBuffers(1, &buffer)); // Создаем буффер и получаем его ID
                               // пока это просто число, никакой памяти не выделено.
 
-    glBindBuffer(GL_ARRAY_BUFFER, buffer); // Делаем этот буфер "активным" для цели GL_ARRAY_BUFFER.
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer)); // Делаем этот буфер "активным" для цели GL_ARRAY_BUFFER.
                                            // Теперь все операции glBufferData / glVertexAttribPointer будут относиться именно к этому буферу.
 
-    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW); // Копируем данные массива positions в видеопамять (в VBO).
+    GLCall(glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW)); // Копируем данные массива positions в видеопамять (в VBO).
                                                                                  // - GL_ARRAY_BUFFER указывает, куда копировать
                                                                                  // - 6 * sizeof(float) = размер данных (здесь 6 float → 12 байт)
                                                                                  // - positions — указатель на данные в RAM, откуда OpenGL заберёт копию
                                                                                  // - GL_STATIC_DRAW — подсказка (hint) драйверу: данные редко меняются
 
-    glEnableVertexAttribArray(0); // Включаем атрибут №0 в VAO.
+    GLCall(glEnableVertexAttribArray(0)); // Включаем атрибут №0 в VAO.
                                   // Это говорит OpenGL: "данный атрибут будет использовать данные из VBO".
                                   // Если не включить — шейдер не будет получать этот атрибут.
 
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0); // Описываем **формат данных**, которые будут читаться из VBO для атрибута №0.
+    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0)); // Описываем **формат данных**, которые будут читаться из VBO для атрибута №0.
                                                                            // Параметры:
                                                                            // (index = 0) — номер атрибута в шейдере (layout(location = 0))
                                                                            // (size = 2) — атрибут состоит из 2 компонентов (например, vec2: x, y)
@@ -155,23 +176,22 @@ int main(void)
 
 
     unsigned int ibo; // индексный буферный обмен
-    glGenBuffers(1, &ibo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+    GLCall(glGenBuffers(1, &ibo));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
 
     ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-    glUseProgram(shader);
+    GLCall(glUseProgram(shader));
 
 /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
-        // отрисовка треугольника
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
-
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr)); // отрисовка треугольника
+       
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
